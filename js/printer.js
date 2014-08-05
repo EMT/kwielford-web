cc4fd901ce5ba3d1e12f14697ddc999420ebc860
Harry Parton <hparton@hotmail.co.uk>
2014-08-05 15:23:39 +0100

    Padding to the top of the printouts.

--- js/printer.js
+++ js/printer.js
@@ -1,6 +1,6 @@
 $(".printer-form").submit(function(e){
 	e.preventDefault();
-	content = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body>'
+	content = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title></head><body><span style="margin-bottom:20px;"></span>'
 	content += $("#content").val();
 	content += '</body></html>'
 	$("#content").val(content);
