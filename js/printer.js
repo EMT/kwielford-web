938ca082caa07cb65aa01e218999913e73f6eda9
Harry Parton <hparton@hotmail.co.uk>
2014-08-06 17:17:53 +0100

    Add to-do list generation to print page.

--- js/printer.js
+++ js/printer.js
@@ -6,3 +6,16 @@ $(".printer-form").submit(function(e){
 	$("#content").val(content);
 	$(".printer-form").submit();
 })
+
+
+$(document).ready(function () {
+	$('.new-to-do').on('click',function(){
+		$('ul').append('<li><p contenteditable="true">Test</p></li>')
+	});
+
+	$('.add').on('click', function(){
+		var test = $('.content').html();
+		console.log(test);
+		$('#content').val(test)
+	});
+});
